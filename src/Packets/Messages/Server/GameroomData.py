5b4d3ccc5818ff0c94fe70ab92d3f35a25b89ca7
Mr Vitalik <50264775+VitalikObject@users.noreply.github.com>
2020-08-14 21:50:41 +0300

    Add files via upload

--- src/Packets/Messages/Server/GameroomData.py
+++ src/Packets/Messages/Server/GameroomData.py
@@ -9,7 +9,9 @@ class GameroomData(Writer):
         self.player = player
 
     def encode(self):
-        self.writeHexa('''01000a0000000000000001b8d9ccc50b0000000f07010100006c7da80e8bad''')
+        self.writeHexa('''01000a0000000000000001b8d9ccc50b0000000f''')
+        self.writeVint(self.player.mapID)
+        self.writeHexa('''010100006c7da80e8bad''')
         self.writeString("Mr Vitalik")
         self.writeHexa('''03100000070702030000000000''')
         print("[INFO] Message GameroomData has been sent.")
