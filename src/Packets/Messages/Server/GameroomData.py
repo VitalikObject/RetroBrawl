cb93ba0e21b26abaa404809ff9e1229d3d9c66f8
Mr Vitalik <50264775+VitalikObject@users.noreply.github.com>
2020-08-15 14:27:25 +0300

    Add files via upload

--- src/Packets/Messages/Server/GameroomData.py
+++ src/Packets/Messages/Server/GameroomData.py
@@ -1,5 +1,5 @@
 from Utils.Writer import Writer
-
+import random
 
 class GameroomData(Writer):
 
@@ -9,9 +9,40 @@ class GameroomData(Writer):
         self.player = player
 
     def encode(self):
-        self.writeHexa('''01000a0000000000000001b8d9ccc50b0000000f''')
+        self.writeVint(1)
+        self.writeVint(0)
+        self.writeVint(10)
+        self.writeVint(0)
+        self.writeVint(0)
+        self.writeVint(0)
+        self.writeVint(0)
+        if(self.player.roomID == 0):
+            self.player.roomID = random.randrange(1, 2147483647)
+            self.writeInt(self.player.roomID)
+        else:
+            self.writeInt(self.player.roomID)
+        self.writeVint(1549375096)
+        self.writeVint(0)
+        self.writeVint(0)
+        self.writeVint(0)
+        self.writeVint(15)
         self.writeVint(self.player.mapID)
-        self.writeHexa('''010100006c7da80e8bad''')
+        self.writeVint(1)
+        self.writeVint(1)
+        self.writeInt(27773)
+        self.writeInt(2819525549)
         self.writeString("<cff3200>M<cff6500>r<cff9800> <cffcb00>V<cffff00>i<cccff00>t<c99ff00>a<c66ff00>l<c33ff00>i<c01ff00>k</c>")
-        self.writeHexa('''03100000070702030000000000''')
+        self.writeVint(3)
+        self.writeVint(16)
+        self.writeVint(0)
+        self.writeVint(0)
+        self.writeVint(7)
+        self.writeVint(7)
+        self.writeVint(2)
+        self.writeVint(3)
+        self.writeVint(0)
+        self.writeVint(0)
+        self.writeVint(0)
+        self.writeVint(0)
+        self.writeVint(0)
         print("[INFO] Message GameroomData has been sent.")
