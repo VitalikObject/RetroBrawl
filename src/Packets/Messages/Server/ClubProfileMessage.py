e14e9216635d1754edf1ff4c67315997b90338ad
Mr Vitalik <50264775+VitalikObject@users.noreply.github.com>
2020-08-17 20:43:17 +0300

    Add files via upload

--- src/Packets/Messages/Server/ClubProfileMessage.py
+++ src/Packets/Messages/Server/ClubProfileMessage.py
@@ -12,7 +12,7 @@ class ClubProfileMessage(Writer):
         self.writeVint(0)
         self.writeInt(0)
         self.writeInt(521)
-        self.writeString("Brawl Private Server")
+        self.writeString("<cff2400>V<cff4800>i<cff6d00>t<cfe9100>a<cffb600>l<cffda00>i<cfffe00>k<cffff00>O<cdaff00>b<cb6ff00>j<c91ff00>e<c6dfe00>c<c48ff00>t</c> <c4>and</c> <cff002a>P<cff0054>h<cff007f>o<cff00a9>e<cff00d4>n<cfe00fe>i<cff00ff>x<cd400ff>F<caa00ff>i<c7f00ff>r<c5500ff>e</c>")
         self.writeVint(8)
         self.writeVint(11)
         self.writeVint(3)
